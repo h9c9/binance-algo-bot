c8b867258ff48d15f484126055c7b469e3e471ad
h9c9 <himanshu.chaube@gmail.com>
2025-07-28 14:09:53 +0530

    Update binancealgotrader.py

--- binancealgotrader.py
+++ binancealgotrader.py
@@ -1,8 +1,8 @@
 import time
 from binance_weighted_algo import BinanceWeightedAlgoTrader
 
-API_KEY = "wQO0PGPi9yLWoLwg0ETK4TM7Qy7AxZ3f4eEfE3B8AzT5yDZtTVZRevC4XO3EK44l"
-SECRET_KEY ="vQdSmkkGhbK2oozLFyDER0x0kla5CpR63Fg2INftD2OlhIR7NcOkiJBD1k9r8Das"
+API_KEY = "wQO0PGPi9yLWoLwg0ETK4TM7Qy7AxZRevC4XO3EK44l"
+SECRET_KEY ="vQdSmkkGhbK2oozLFyDER7NcOkiJBD1k9r8Das"
 
 if __name__ == "__main__":
     bot = BinanceWeightedAlgoTrader(API_KEY, SECRET_KEY)
